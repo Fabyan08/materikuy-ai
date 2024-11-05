673328c69ed66a0b4202faaedf474354fb5cc2e3
Fabyan Yastika Permana <fabyanxrpl@gmail.com>
2024-11-05 17:43:53 +0700

    fix error

--- src/db/migrations/schema.ts
+++ src/db/migrations/schema.ts
@@ -1,5 +1,4 @@
-import { pgTable, pgEnum } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
+import {  pgEnum } from "drizzle-orm/pg-core"
 
 export const durasiBelajar = pgEnum("durasi_belajar", ['Seminggu', 'Satu bulan', 'Lebih dari satu bulan'])
 export const tingkat = pgEnum("tingkat", ['Pemula', 'Menengah', 'Sepuh'])
